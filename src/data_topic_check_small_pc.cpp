92adc15444ee003e99bcba5e0bd19df8f7a5e734
gogojjh <jiaojh1994@gmail.com>
2023-06-09 00:43:40 +0800

    update the check of encoder

--- src/data_topic_check_small_pc.cpp
+++ src/data_topic_check_small_pc.cpp
@@ -8,6 +8,7 @@
 #include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/Imu.h>
+#include <sensor_msgs/JointState.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <std_srvs/Empty.h>
@@ -26,6 +27,7 @@ double latest_gps_time = 0.0;
 double latest_gps_3dm_time = 0.0;
 double latest_event_cam00_time = 0.0;
 double latest_event_cam01_time = 0.0;
+double latest_encoder_time = 0.0;
 // ******************************
 
 void LiDARCallback(const sensor_msgs::PointCloud2ConstPtr& msg) {
@@ -60,6 +62,10 @@ void GPS3DMCallback(const sensor_msgs::NavSatFixConstPtr& msg) {
   latest_gps_3dm_time = msg->header.stamp.toSec();
 }
 
+void EncoderCallback(const sensor_msgs::JointStateConstPtr& msg) {
+  latest_encoder_time = msg->header.stamp.toSec();
+}
+
 void CheckData(const double& curr_time, const double sensor_time,
                const std::string info) {
   if (sensor_time >= 0 &&
@@ -75,16 +81,18 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
 
-  if (argc == 5) {
+  if (argc == 6) {
     std::string topic1(argv[1]);
     std::string topic2(argv[2]);
     std::string topic3(argv[3]);
     std::string topic4(argv[4]);
+    std::string topic5(argv[5]);
     std::cout << "Topic lists: " << std::endl;
     std::cout << "--- lidar topic: " << topic1 << std::endl;
     std::cout << "--- event_cam00 topic: " << topic2 << std::endl;
     std::cout << "--- event_cam01 topic: " << topic3 << std::endl;
     std::cout << "--- 3dm imu topic: " << topic4 << std::endl;
+    std::cout << "--- encoder topic: " << topic5 << std::endl;
 
     ros::Subscriber lidar_sub =
         nh.subscribe<sensor_msgs::PointCloud2>(topic1, 5, LiDARCallback);
@@ -96,6 +104,8 @@ int main(int argc, char** argv) {
                                                    EventCam01Callback);
     ros::Subscriber imu_sub =
         nh.subscribe<sensor_msgs::Imu>(topic4, 5, IMUCallback);
+    ros::Subscriber encoder_sub =
+        nh.subscribe<sensor_msgs::JointState>(topic4, 5, EncoderCallback);
 
     double latest_curr_time = ros::Time::now().toSec();
 
@@ -109,6 +119,7 @@ int main(int argc, char** argv) {
         CheckData(latest_event_cam00_time, latest_curr_time, "event_cam00");
         CheckData(latest_event_cam01_time, latest_curr_time, "event_cam01");
         CheckData(latest_imu_time, latest_curr_time, "imu");
+        CheckData(latest_encoder_time, latest_curr_time, "wheel_encoder");
       }
       loop_rate.sleep();
     }
